aaf7edecd492614b16fa46e6f960a7e027bf0fb4
Yash <yjdobariya0@gmail.com>
2026-01-04 13:42:24 +0530

    removed skill from home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Skills from "./components/Skills";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
@@ -17,10 +16,7 @@ function App() {
       <Projects />
       <Education />
       <About />
-
-      <Skills />
       <Contact />
-
       <SocialLinks />
     </div>
   );
